67a261098b4d4637576c0f4ac4c28d08b1976047
BrandOtter <160093135+BrandOtter@users.noreply.github.com>
2024-03-12 08:33:02 -0700

    Fixed EnrollmentRepository
    Added necessary code for SpringBoot to register and accept the queries within the EnrollmentRepository

--- src/main/java/com/cst438/domain/EnrollmentRepository.java
+++ src/main/java/com/cst438/domain/EnrollmentRepository.java
@@ -10,10 +10,10 @@ public interface EnrollmentRepository extends CrudRepository<Enrollment, Integer
     @Query(value = "select e from Enrollment e where e.section.sectionNo=:sectionNo order by e.student.name", nativeQuery = true)
     List<Enrollment> findEnrollmentsBySectionNoOrderByStudentName(int sectionNo);
 
-    @Query("select e from Enrollment e where e.student.id=:studentId order by e.section.term.termId")
+    @Query(value = "select e from Enrollment e where e.student.id=:studentId order by e.section.term.termId", nativeQuery = true)
     List<Enrollment> findEnrollmentsByStudentIdOrderByTermId(int studentId);
 
-    @Query("select e from Enrollment e where e.section.term.year=:year and e.section.term.semester=:semester and e.student.id=:studentId order by e.section.course.courseId")
+    @Query(value = "select e from Enrollment e where e.section.term.year=:year and e.section.term.semester=:semester and e.student.id=:studentId order by e.section.course.courseId", nativeQuery = true)
     List<Enrollment> findByYearAndSemesterOrderByCourseId(int year, String semester, int studentId);
 
     //@Query("select e from Enrollment e where e.section.sectionNo=:sectionNo and e.student.id=:studentId")
